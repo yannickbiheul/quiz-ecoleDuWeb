65d3700a12773b78e24a55445a487fff6051bd74
Yannick Biheul <yannickbiheul@outlook.fr>
2020-09-27 16:04:10 +0200

    JavaScript 2ème question

--- js/quiz.js
+++ js/quiz.js
@@ -13,7 +13,6 @@ let userReponse1 = "";
 for (let i = 0; i < reponsesQ1.length; i++) {
     reponsesQ1[i].addEventListener("click", function() {
         userReponse1 = reponsesQ1[i].value;
-        console.log(userReponse1);
         if (userReponse1 == bonneReponse1q1) {
             question[0].style.backgroundColor = "var(--green)";
         } else {
@@ -22,3 +21,22 @@ for (let i = 0; i < reponsesQ1.length; i++) {
     })
 }
 
+// QUESTION 2
+let reponse1q2 = document.getElementById("q2r1");
+let reponse2q2 = document.getElementById("q2r2");
+let reponse3q2 = document.getElementById("q2r3");
+let reponse4q2 = document.getElementById("q2r4");
+let reponsesQ2 = [reponse1q2, reponse2q2, reponse3q2, reponse4q2];
+let bonneReponse1q2 = "q2r4";
+let userReponse2 = "";
+
+for (let i = 0; i < reponsesQ2.length; i++) {
+    reponsesQ2[i].addEventListener("click", function () {
+        userReponse2 = reponsesQ2[i].value;
+        if (userReponse2 == bonneReponse1q2) {
+            question[1].style.backgroundColor = "var(--green)";
+        } else {
+            question[1].style.backgroundColor = "var(--red)";
+        }
+    })
+}
